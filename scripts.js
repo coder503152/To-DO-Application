ad22043491658ec23b435898f625f450669bb782
Ayush <ak503152@gmail.com>
2025-05-27 16:27:29 +0530

    final change

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-let ct = 1
+let ctr = 1
 
+function deletetodo(ctr) {
+    const eltodel = document.getElementById("to-do" + ctr);
+    eltodel.parentNode.removeChild(eltodel);
+}
 
 function addtodo() {
     const content = document.querySelector("#text");
     const newEl = document.createElement("div");
-    newEl.innerHTML = ct +"> " +  content.value ;
-    ct++;
+    newEl.setAttribute("id" , "to-do" + ctr);
+
+    newEl.innerHTML = '<div>' + ctr + '>' + content.value+ '</div>' + '<button onclick="deletetodo(ctr)">Delete</button> ' ;
+    // ct++;
     const parEl = document.querySelector("body").appendChild(newEl);
+    ctr++;
 }
 
-
-function deletetodo() {
-    const listdiv = document.querySelectorAll("div");
-    let size = listdiv.length;
-    const eltodel = listdiv[size-1];
-    document.querySelector("body").removeChild(eltodel);
-}
