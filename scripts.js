ec36e99624a6425f7d0b5c8631d6d7c5d3999897
Ayush <ak503152@gmail.com>
2025-05-27 16:29:53 +0530

    Initial commits

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function addtodo() {
     const newEl = document.createElement("div");
     newEl.setAttribute("id" , "to-do" + ctr);
 
-    newEl.innerHTML = '<div>' + ctr + '>' + content.value+ '</div>' + '<button onclick="deletetodo(ctr)">Delete</button> ' ;
+    newEl.innerHTML = '<div>' + ctr + '>' + content.value+ '</div>' + '<button onclick="deletetodo(' +ctr+ ')">Delete</button> ' ;
     // ct++;
     const parEl = document.querySelector("body").appendChild(newEl);
     ctr++;
